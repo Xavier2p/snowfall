904b93477de0f0d3211722eb552f999522a36ba8
Xavier de Place <xavier.de-place@epita.fr>
2025-02-12 10:43:08 +0100

    fix(flake): allow unfree packages

--- flake.nix
+++ flake.nix
@@ -13,7 +13,10 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
       in {
         inherit pkgs;
         devShells = {
